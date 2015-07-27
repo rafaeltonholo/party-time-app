a2cfee0b71235df9af7cea35b165a75711e098e0
Rafael R. Tonholo <rafaelrtonholo@gmail.com>
2015-07-26 22:24:39 -0300

    Refatorado serviços

--- app/www/js/services.js
+++ app/www/js/services.js
@@ -1,9 +1,11 @@
+// Tornar o objeto api global.
+var api = new ServerAPI();
 
 angular.module('partyTimeApp.services', [])
 
    //Login service usado para fornecer funções de login
   .factory("LoginService", ["$http", function ($http) {
-    var api = new ServerAPI();
+            
     return {
       singup: function (data) {
         return $http.post(api.pessoa, data, postConfig)
@@ -18,7 +20,7 @@ angular.module('partyTimeApp.services', [])
 .factory("PerfilService", ["$http", function($http) {
     return {
         getConvitesPendentes: function(data) {
-            return $http.get(SERVER_ADDRESS + "/party/pessoa/" + data + "/convite/pendentes", {
+            return $http.get(api.pessoa + "/" + data + "/convite/pendentes", {
                 header: {
                     "Content-Type": "application/json"
                 }
@@ -33,14 +35,14 @@ angular.module('partyTimeApp.services', [])
 .factory("ConviteService", ["$http", function($http) {
     return { 
         getConvites: function(data) {
-            return $http.get(SERVER_ADDRESS + "/party/pessoa/" + data.pessoa_id + "/convite/", {
+            return $http.get(api.pessoa + "/" + data.pessoa_id + "/convite/", {
                 header: {
                     "Content-Type": "application/json"
                 }
             });
         },
         aceitar: function(data) {
-            return $http.put(SERVER_ADDRESS + "/party/pessoa/" + data.pessoa_id + "/convite/" + data.convite_id, data.data, {
+            return $http.put(api.pessoa + "/" + data.pessoa_id + "/convite/" + data.convite_id, data.data, {
                 header: {
                     "Content-Type": "application/json"
                 }
@@ -48,21 +50,56 @@ angular.module('partyTimeApp.services', [])
         }
     };
 }])
+.factory('Chats', function () {
+    // Might use a resource here that returns a JSON array
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+    // Some fake testing data
+    var chats = [{
+      id: 0,
+      name: 'Ben Sparrow',
+      lastText: 'You on your way?',
+      face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    }, {
+        id: 1,
+        name: 'Max Lynx',
+        lastText: 'Hey, it\'s me',
+        face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+      }, {
+        id: 2,
+        name: 'Adam Bradleyson',
+        lastText: 'I should buy a boat',
+        face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+      }, {
+        id: 3,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+      }, {
+        id: 4,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+      }];
 
-
-        }
+    return {
+      all: function () {
+        return chats;
+      },
+      remove: function (chat) {
+        chats.splice(chats.indexOf(chat), 1);
+      },
+      get: function (chatId) {
+        for (var i = 0; i < chats.length; i++) {
+          if (chats[i].id === parseInt(chatId)) {
+            return chats[i];
+          }
+        };
         return null;
       }
-      return null;
-    }
-  };
-});
+      
     };
   });
-  
+
 var postConfig = {
   header: {
     "Content-Type": "application/json"
